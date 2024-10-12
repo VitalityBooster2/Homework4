ec4e9b4f3376b07f2d52be9ec0810d6049132d7a
VitalityBooster <vitalik.oganyan89884076959@gmail.com>
2024-10-12 23:07:10 +0300

    'Fileadd'

---
+++ hello.pas
@@ -0,0 +1,22 @@
+﻿begin 
+  write('Введите накаком языке вы разговариваете: ');
+  var language : string ;
+  Readln(language) ;
+  
+  
+  case language of 
+    'Английский' : Print('Hello');
+    'Русский' : Print('Привет');
+    'Французский' : Print('Bonjour');
+  end;
+    
+    
+    
+    
+
+  
+  
+  
+  
+  
+end.
